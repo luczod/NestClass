3acf038debf130e4ccc5c0fd8e5c71bcb1048049
luczod <kzod5611@gmail.com>
2023-11-14 16:53:44 -0300

    dto initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+//npx nest g controller 'name'
+//npx nest g module 'name'
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
