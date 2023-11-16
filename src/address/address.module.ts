a4944865222b4beeb608437cdf17f21e2e85e442
luczod <kzod5611@gmail.com>
2023-11-16 17:21:34 -0300

    controller address

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressController } from './address.controller';
+import { AddressService } from './address.service';
+import { AddressEntity } from './model/address.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  controllers: [AddressController],
+  providers: [AddressService],
+})
 export class AddressModule {}
