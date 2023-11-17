93ede64feaf8b553d81e3fa154215a7125afda26
luczod <kzod5611@gmail.com>
2023-11-17 19:10:14 -0300

    check jwt

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,15 +1,11 @@
-import {
-  Body,
-  Controller,
-  Param,
-  Post,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './model/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/userType.enum';
 
+@Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
