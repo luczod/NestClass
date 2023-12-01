45d0dd4d850477854083d388c68619b59041940c
luczod <kzod5611@gmail.com>
2023-12-01 12:27:58 -0300

    payment service complement

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,15 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { OrderEntity } from './model/order.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderDTO } from './dtos/create-order.dto';
+import { PaymentService } from '../payment/payment.service';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(OrderEntity)
     private readonly orderEntity: Repository<OrderEntity>,
+    private readonly paymentService: PaymentService,
   ) {}
 
   async createOrder(createOrderDTO: CreateOrderDTO, cartId: number) {
+    await this.paymentService.createPayment(createOrderDTO);
     return null;
   }
 }
