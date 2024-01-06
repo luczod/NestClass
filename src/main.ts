13c84ebb041df0f40fc05d6cebe8d51b120908c6
luczod <kzod5611@gmail.com>
2024-01-06 08:04:24 -0300

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import metadata from './metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce example')
+    .setDescription('The Ecommerce API reactNative app')
+    .setVersion('1.0')
+    .build();
+  await SwaggerModule.loadPluginMetadata(metadata);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
